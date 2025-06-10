cd8ed48590faff95716dfa0a13b8aa29555ecd7b
hasandurna <98964767+hasandurna@users.noreply.github.com>
2025-06-10 19:08:59 +0300

    Update uart.h

--- uart.h
+++ uart.h
@@ -11,6 +11,19 @@ extern "C" {
 	 UART_3,
  } UART_PERIPH;
 
+enum {
+   UART_ERR_NOISE = -1,
+   UART_ERR_OVERRUN = -2,
+   UART_ERR_FRAMING = -3,
+   UART_ERR_PARITY = -4,
+};
+
+void UART_Init(UART_PERIPH idx, int baud);
+void UART_Send(UART_PERIPH idx, unsigned char val);
+
+int UART_DataReady(UART_PERIPH idx);
+int UART_Recv(UART_PERIPH idx);
+
 
 #ifdef __cplusplus
 }
