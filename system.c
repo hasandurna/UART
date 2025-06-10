b396d07446fa4ad9c26c108353107e94f255c367
hasandurna <98964767+hasandurna@users.noreply.github.com>
2025-06-10 19:09:03 +0300

    Update system.c

--- system.c
+++ system.c
@@ -9,4 +9,23 @@
 #define _STDOUT 1
 #define _STDERR 2
 
+UART_PERIPH _conUART = UART_1;
+
+
+void Sys_ClockTick(void)
+{
+  ++_TmTick;
+}
+
+void Sys_ConsoleInit(void)
+{
+  LCD_Init();
+  // UART_Init(_conUART, 115200);
+}
+
+void _putch(unsigned char c)
+{
+  LCD_putch(c);
+  // UART_putch(_conUART, c);
+}
 
